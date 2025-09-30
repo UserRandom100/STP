e03b84c8458c5f66cd679c2252f22be6bb9dda90
UserRandom100 <usercoding100@gmail.com>
2025-09-30 16:30:19 -0400

    logo click-able

--- web/vues/inclusions/logo.inc.php
+++ web/vues/inclusions/logo.inc.php
@@ -1,4 +1,6 @@
 <div class="Logo">
-  Swipe Ton Souper
-  <!-- <img src="./assets/img/logo.jpeg" alt="Logo de SwipeTonSouper"> -->
+    <a href="?action=<?php echo isset($_SESSION['utilisateurConnecte']) ? 'bienvenue' : 'seConnecter'; ?>">
+        Swipe Ton Souper
+    </a>
+    <!-- <img src="./assets/img/logo.jpeg" alt="Logo de SwipeTonSouper"> -->
 </div>
